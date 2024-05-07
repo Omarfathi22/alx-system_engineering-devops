4bef324e372a87a512b255d506ccefa268ad6153
Omar Fathi22 <ommarf525@gmail.com>
2024-05-07 10:55:07 -0700

    1-repetition_token_0.rb0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed to the script
-# Define the regular expression pattern
-# Match the pattern in the input
-# Print the matched string or empty if not found
-input = ARGV[0]
-pattern = /School+/
-match = input.match(pattern)
-puts match ? match[0] : ""
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
