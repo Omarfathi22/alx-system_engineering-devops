5070e390e767d220054132bc7ee48db45fe0806f
Omar Fathi22 <ommarf525@gmail.com>
2024-05-07 11:11:59 -0700

    fixed

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,7 +4,4 @@
 # Define the regular expression pattern
 # Match the pattern in the input
 # Print the matched string or empty if not found
-input = ARGV[0]
-pattern = /School/
-match = input.match(pattern)
-puts match ? match[0] : ""
+puts ARGV[0].scan(/School/).join
