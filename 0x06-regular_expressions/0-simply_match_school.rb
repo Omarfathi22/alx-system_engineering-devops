dc89df31379186f6cad534b6e2d1dda6b197de8d
Omar Fathi22 <ommarf525@gmail.com>
2024-05-07 10:28:47 -0700

    0-simply_match_school.rb

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# Get the argument passed to the script
+# Define the regular expression pattern
+# Match the pattern in the input
+# Print the matched string or empty if not found
+input = ARGV[0]
+pattern = /School/
+match = input.match(pattern)
+puts match ? match[0] : ""
