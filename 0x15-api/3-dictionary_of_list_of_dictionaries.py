0952327ceb01850052356687ee4fe7c87a21b3b8
Omar Fathi22 <ommarf525@gmail.com>
2024-07-29 11:40:46 -0700

    fixed

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,45 +2,45 @@
 import json
 import requests
 
-def fetch_data():
-    # Fetch users
-    users_response = requests.get('https://jsonplaceholder.typicode.com/users')
-    users = users_response.json()
+def fetch_data(url):
+    """Fetch data from a given URL."""
+    response = requests.get(url)
+    return response.json()
 
-    # Fetch todos
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos_response.json()
+def main():
+    # Base URL for the API
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-    return users, todos
+    # Fetch all users
+    users = fetch_data(f'{base_url}/users')
 
-def create_data_structure(users, todos):
-    data = {}
+    # Dictionary to store all tasks of all employees
+    all_tasks = {}
+
     for user in users:
         user_id = user['id']
         username = user['username']
+
+        # Fetch all tasks for the current user
+        todos = fetch_data(f'{base_url}/todos?userId={user_id}')
+
+        # List to store the tasks in the required format
         user_tasks = []
 
         for todo in todos:
-            if todo['userId'] == user_id:
-                task_info = {
-                    "username": username,
-                    "task": todo['title'],
-                    "completed": todo['completed']
-                }
-                user_tasks.append(task_info)
+            task_info = {
+                'username': username,
+                'task': todo['title'],
+                'completed': todo['completed']
+            }
+            user_tasks.append(task_info)
 
-        data[user_id] = user_tasks
+        # Add the user's tasks to the dictionary
+        all_tasks[user_id] = user_tasks
 
-    return data
-
-def export_to_json(data):
+    # Write the dictionary to a JSON file
     with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(data, json_file)
-
-def main():
-    users, todos = fetch_data()
-    data = create_data_structure(users, todos)
-    export_to_json(data)
+        json.dump(all_tasks, json_file)
 
 if __name__ == '__main__':
     main()
