af60e5e852b6c78bede1ad46c0b7a98917591b1e
Omar Fathi22 <ommarf525@gmail.com>
2024-07-29 13:35:06 -0700

    fix -3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,55 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""
+This script fetches data from a REST API and saves it to a JSON file.
+It exports all employees' tasks in a JSON format.
+"""
 import json
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+def fetch_data():
+    """Fetches user and task data from the REST API"""
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    users_response = requests.get(users_url)
+    todos_response = requests.get(todos_url)
+
+    users = users_response.json()
+    todos = todos_response.json()
+
+    return users, todos
+
+
+def format_data(users, todos):
+    """Formats data into a dictionary of lists of dictionaries"""
+    data = {}
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+        user_tasks = [task for task in todos if task['userId'] == user_id]
+        tasks_list = [{
+            "username": username,
+            "task": task['title'],
+            "completed": task['completed']
+        } for task in user_tasks]
+        data[user_id] = tasks_list
+
+    return data
+
+
+def save_to_json(data, filename='todo_all_employees.json'):
+    """Saves the data to a JSON file"""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def main():
+    """Main function"""
+    users, todos = fetch_data()
+    data = format_data(users, todos)
+    save_to_json(data)
+
+
+if __name__ == "__main__":
+    main()
