fef0b84687e7545d996fa6401711a20de9fe4724
Omar Fathi22 <ommarf525@gmail.com>
2024-08-13 16:08:51 -0700

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,19 +5,20 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    headers = {'User-Agent': 'my-reddit-api'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        elif response.status_code == 302:  # Handle redirect
-            return 0
-        else:
-            return 0
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
