e7df2bae35b9f27aa3276ec4da5dbe15ace862c9
Omar Fathi22 <ommarf525@gmail.com>
2024-08-22 23:54:04 -0700

    fixed

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,9 @@
+# This Puppet manifest modifies the file descriptor limits for the holberton user.
+
 exec { 'change-os-configuration-for-holberton-user':
-  command => "bash -c \"sed -iE 's/^holberton hard nofile 5/holberton hard nofile 88888/' /etc/security/limits.conf; \
-sed -iE 's/^holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
-  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'bash -c "sed -iE \'s/^holberton hard nofile 5/holberton hard nofile 88888/\' /etc/security/limits.conf; \
+               sed -iE \'s/^holberton soft nofile 4/holberton soft nofile 88888/\' /etc/security/limits.conf"',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
   unless  => "grep -q 'holberton hard nofile 88888' /etc/security/limits.conf && grep -q 'holberton soft nofile 88888' /etc/security/limits.conf",
 }
 
